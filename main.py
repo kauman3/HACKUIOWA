c8cf9d8534f4b7ee0d25eaec7f3ba36ca6761236
kauman3 <69825322+kauman3@users.noreply.github.com>
2021-04-17 20:42:28 -0500

    added basic functions and added 'report' and 'rebuild' to kivy

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 screen_manager = """
 ScreenManager:
     Home:
+    Rebuild:
     Vacation:
+    Report:
     
     
 <Home>:
@@ -14,7 +16,7 @@ ScreenManager:
     BoxLayout:
         orientation: 'vertical'
         Label:
-            text: 'Selected one of the following to plan:'
+            text: 'Select one of the following plans:'
         Button:
             text: 'Plan Future Vacation'
             on_press: root.manager.current = 'vacation'
@@ -22,11 +24,76 @@ ScreenManager:
             pos_hint: {'x': .25}
         Button:
             text: 'Plan Rebuild'
-            on_press: root.manager.current = 'vacation'
+            on_press: root.manager.current = 'rebuild'
             size_hint: (.5,.5)
             pos_hint: {'x': .25}
             
-    
+
+<Rebuild>:
+    name: 'rebuild'
+    BoxLayout:
+        orientation: 'vertical'
+        Button:
+            size_hint: (.1,.1)
+            pos_hint: {'x': 0, 'top': .1}
+            text: '<'
+            font_size: 30
+            on_press: root.manager.current = 'home'
+        BoxLayout:
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    text: 'Name of (Project)'
+                    size_hint: (.5,.1)
+        
+                TextInput:
+                    id: name
+                    size_hint: (.5,.1)
+                    
+                Label:
+                    text: 'How many Weeks/Months do you want to pay for'
+                    size_hint: (.7,.1)
+                Spinner:
+                    id: weeks_months
+                    values: 'weeks', 'month', 'year'
+                    size_hint: (.5,.1)
+        
+                Label:
+                    text: 'For how long'
+                    size_hint: (.5,.1)
+                TextInput:
+                    id: length
+                    size_hint: (.5,.1)
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    text: 'Your Salary'
+                    size_hint: (.5,.1)
+        
+                TextInput:
+                    id: salary
+                    size_hint: (.5,.1)
+                    
+                Label:
+                    text: 'Total cost of (Project)'
+                    size_hint: (.5,.1)
+        
+                TextInput:
+                    id: total_cost
+                    size_hint: (.5,.1)
+                    
+                Label:
+                    text: ''
+                    size_hint: (.5,.1)
+                
+                Button:
+                    text: 'Click to Get Report'
+                    size_hint: (.5,.1)
+                    pos_hint: {'top': .1}
+                    on_press: root.update_variables()
+                    on_press: root.manager.current = 'report'
+
+
 <Vacation>:
     name: 'vacation'
     BoxLayout:
@@ -81,21 +148,88 @@ ScreenManager:
                     size_hint: (.5,.1)
                     
                 Label:
-                    text: 'Save'
+                    text: ''
                     size_hint: (.5,.1)
                 
                 Button:
-                    text: 'Save'
+                    text: 'Click to Get Report'
                     size_hint: (.5,.1)
                     pos_hint: {'top': .1}
                     on_press: root.update_variables()
+                    on_press: root.manager.current = 'report'
                
         
+<Report>:
+    name: 'report'
+    BoxLayout:
+        orientation: 'vertical'
+        BoxLayout:
+            orientation: 'horizontal'
+            Label:
+                text: 'Weekly Payment:'
+                size_hint: (.5,.1)
+            Label:
+                text: 'app.weekly_payment'
+                size_hint: (.5,.1)
+        BoxLayout:
+            orientation: 'horizontal'
+            Label:
+                text: 'Monthly Payment:'
+                size_hint: (.5,.1)
+            Label:
+                text: 'app.monthly_payment'
+                size_hint: (.5,.1)
+        BoxLayout:
+            orientation: 'horizontal'
+            Label:
+                text: 'Yearly Payment:'
+                size_hint: (.5,.1)
+            Label:
+                text: 'app.yearly_payment'
+                size_hint: (.5,.1)
+        BoxLayout:
+            orientation: 'horizontal'
+            Label:
+                text: 'Percentage of Salary:'
+                size_hint: (.5,.1)
+            Label:
+                text: 'app.salary_percentage'
+                size_hint: (.5,.1)
+    
+    
 """
 
 
 class Home(Screen):
     pass
+
+
+class Rebuild(Screen):
+    def update_variables(self):
+        app.salary = int(self.ids.salary.text)
+        app.total_cost = int(self.ids.total_cost.text)
+        app.weeks_months_years = self.ids.weeks_months.text
+        app.length_of_payments = int(self.ids.length.text)
+
+    def get_days(self):
+        if app.weeks_months_years == 'weeks':
+            return app.length_of_payments * 7
+        elif app.weeks_months_years == 'months':
+            return app.length_of_payments * 30
+        elif app.weeks_months_years == 'years':
+            return app.length_of_payments * 365.25
+
+    def get_weekly_payment(self):
+        app.weekly_payments = str(app.total_cost / (self.get_days() / 7))
+
+    def get_monthly_payment(self):
+        app.monthly_payments = str(app.total_cost / (self.get_days() / 30))
+
+    def yearly_payments(self):
+        app.yearly_payments = str(app.total_cost / (self.get_days() / 30))
+
+    def get_salary_percentage(self):
+        app.salary_percentage = str(app.total_cost / app.salary)
 
 
 class Vacation(Screen):
@@ -105,10 +239,36 @@ class Vacation(Screen):
         app.weeks_months_years = self.ids.weeks_months.text
         app.length_of_payments = int(self.ids.length.text)
 
+    def get_days(self):
+        if app.weeks_months_years == 'weeks':
+            return app.length_of_payments * 7
+        elif app.weeks_months_years == 'months':
+            return app.length_of_payments * 30
+        elif app.weeks_months_years == 'years':
+            return app.length_of_payments * 365.25
+
+    def get_weekly_payment(self):
+        app.weekly_payments = str(app.total_cost / (self.get_days() / 7))
+
+    def get_monthly_payment(self):
+        app.monthly_payments = str(app.total_cost / (self.get_days() / 30))
+
+    def yearly_payments(self):
+        app.yearly_payments = str(app.total_cost / (self.get_days() / 30))
+
+    def get_salary_percentage(self):
+        app.salary_percentage = str(app.total_cost / app.salary)
+
+
+class Report(Screen):
+    pass
+
 
 manage = ScreenManager()
 manage.add_widget(Home(name='home'))
+manage.add_widget(Rebuild(name='rebuild'))
 manage.add_widget(Vacation(name='vacation'))
+manage.add_widget(Report(name='report'))
 
 
 class Budget(App):
@@ -116,6 +276,11 @@ class Budget(App):
     total_cost = NumericProperty()
     weeks_months_years = StringProperty()
     length_of_payments = NumericProperty()
+    weekly_payments = StringProperty()
+    monthly_payments = StringProperty()
+    yearly_payments = StringProperty()
+    salary_percentage = StringProperty()
+
 
     def build(self):
         screen = Builder.load_string(screen_manager)
